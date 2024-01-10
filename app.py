ea012eb2a93eea8fb9d56471d3ea1774c789f1aa
Yuliia <frogshapedanalyticalengine@gmail.com>
2024-01-10 19:38:50 +0200

    added metatags

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import random
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/vent', methods=['POST'])
+def vent():
+    messages = [
+        "Life is tough sometimes.",
+        "I just need to get this off my chest.",
+        "Feeling overwhelmed...",
+        "Anybody else having a rough day?"
+    ]
+
+    message = random.choice(messages)
+    user_input = request.form.get('user_input')
+
+    return jsonify({'message': message, 'user_input': user_input})
 
 if __name__ == '__main__':
     app.run(debug=True)
